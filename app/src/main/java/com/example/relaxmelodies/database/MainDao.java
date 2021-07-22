7cdbc4c3fd62ad00c52ebc6c8b58a75718b58103
ShayanTalaei <talaee.shayan@gmail.com>
2021-07-22 14:00:53 +0430

    MainDao interface

--- app/src/main/java/com/example/relaxmelodies/database/MainDao.java
+++ app/src/main/java/com/example/relaxmelodies/database/MainDao.java
@@ -5,8 +5,37 @@ import androidx.room.Delete;
 import androidx.room.Insert;
 import androidx.room.Query;
 
+import java.util.List;
+
+import static androidx.room.OnConflictStrategy.REPLACE;
+
 @Dao
 public interface MainDao {
 
+    @Insert(onConflict = REPLACE)
+    void insert(Melody melody);
 
+    @Insert(onConflict = REPLACE)
+    void insert(SavedMix savedMix);
+
+    @Delete
+    void delete(Melody melody);
+
+    @Delete
+    void delete(SavedMix savedMix);
+
+    @Query("DELETE FROM Melody")
+    void deleteAllMelodies();
+
+    @Query("DELETE FROM SavedMix")
+    void deleteAllSavedMixes();
+
+    @Query("SELECT * FROM Melody")
+    List<Melody> getAllMelodies();
+
+    @Query("SELECT * FROM SavedMix")
+    List<SavedMix> getAllSavedMixes();
+
+    @Query("SELECT * FROM SavedMix WHERE name LIKE :name ")
+    List<SavedMix> findSavedMixByName(String name);
 }
