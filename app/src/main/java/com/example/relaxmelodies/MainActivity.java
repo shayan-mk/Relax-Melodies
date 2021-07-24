8ca50a7da3467deacfd121401b332e77101a5964
Shayan Mohammadi <shayan.mkk@gmail.com>
2021-07-24 16:01:30 +0430

    cleaned up a little bit

--- app/src/main/java/com/example/relaxmelodies/MainActivity.java
+++ app/src/main/java/com/example/relaxmelodies/MainActivity.java
@@ -2,8 +2,6 @@ package com.example.relaxmelodies;
 
 import android.os.Bundle;
 
-import com.google.android.material.bottomnavigation.BottomNavigationView;
-
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
@@ -11,18 +9,19 @@ import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
 import com.example.relaxmelodies.databinding.ActivityMainBinding;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
     //Defining Message Codes
     private static final int BASE_MESSAGE_CODE = 1000;
-    public static final int DB_MELODY_LOAD =        BASE_MESSAGE_CODE + 1;
-    public static final int DB_SAVED_MIX_LOAD =     BASE_MESSAGE_CODE + 2;
-    public static final int DB_MELODY_INSERT =      BASE_MESSAGE_CODE + 3;
-    public static final int DB_SAVED_MIX_INSERT =   BASE_MESSAGE_CODE + 4;
-    public static final int DB_MELODY_DELETE =      BASE_MESSAGE_CODE + 5;
-    public static final int DB_SAVED_MIX_DELETE =   BASE_MESSAGE_CODE + 6;
-    public static final int DB_MELODY_TRUNCATE =    BASE_MESSAGE_CODE + 7;
+    public static final int DB_MELODY_LOAD = BASE_MESSAGE_CODE + 1;
+    public static final int DB_SAVED_MIX_LOAD = BASE_MESSAGE_CODE + 2;
+    public static final int DB_MELODY_INSERT = BASE_MESSAGE_CODE + 3;
+    public static final int DB_SAVED_MIX_INSERT = BASE_MESSAGE_CODE + 4;
+    public static final int DB_MELODY_DELETE = BASE_MESSAGE_CODE + 5;
+    public static final int DB_SAVED_MIX_DELETE = BASE_MESSAGE_CODE + 6;
+    public static final int DB_MELODY_TRUNCATE = BASE_MESSAGE_CODE + 7;
     public static final int DB_SAVED_MIX_TRUNCATE = BASE_MESSAGE_CODE + 8;
 
     private ActivityMainBinding binding;
@@ -34,7 +33,7 @@ public class MainActivity extends AppCompatActivity {
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-        BottomNavigationView navView = findViewById(R.id.nav_view);
+        BottomNavigationView navView = binding.navView;
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
@@ -47,5 +46,4 @@ public class MainActivity extends AppCompatActivity {
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(binding.navView, navController);
     }
-
 }
