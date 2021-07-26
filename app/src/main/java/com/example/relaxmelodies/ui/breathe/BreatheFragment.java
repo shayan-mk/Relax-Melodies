80705a11d3f681a5fff59408d0923fab574ef23a
ShayanTalaei <talaee.shayan@gmail.com>
2021-07-27 01:49:14 +0430

    ZoomIn and ZoomOut animation

--- app/src/main/java/com/example/relaxmelodies/ui/breathe/BreatheFragment.java
+++ app/src/main/java/com/example/relaxmelodies/ui/breathe/BreatheFragment.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -12,12 +14,22 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.example.relaxmelodies.MainActivity;
+import com.example.relaxmelodies.R;
 import com.example.relaxmelodies.databinding.FragmentBreatheBinding;
 
 public class BreatheFragment extends Fragment {
 
     private BreatheViewModel breatheViewModel;
     private FragmentBreatheBinding binding;
+
+    private Animation animationInhaleText;
+    private Animation animationHoldText;
+    private Animation animationExhaleText;
+
+    private View outerCircleView;
+    private View innerCircleView;
+
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -44,4 +56,14 @@ public class BreatheFragment extends Fragment {
     }
 
 
+    private void prepareAnimation(){
+        int inhaleDuration = breatheViewModel.getInhaleDuration();
+        int exhaleDuration = breatheViewModel.getExhaleDuration();
+        
+        
+        //Inhale - make large
+        animationInhaleText = AnimationUtils.loadAnimation((MainActivity)getActivity(), R.anim.zoomin);
+        
+        
+    }
 }
